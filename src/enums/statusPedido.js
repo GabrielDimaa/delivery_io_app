2ccd885e49f6dc248737ba0673fd6dd00cb67019
Gabriel de Matos Hainzenreder <44183128+GabrielDimaa@users.noreply.github.com>
2022-02-15 00:02:03 -0300

    Enum Status pedido

---
+++ src/enums/statusPedido.js
@@ -0,0 +1,8 @@
+export default Object.freeze({
+    EmAberto: {value: 1, descricao: "Em aberto"},
+    Aceito: {value: 2, descricao: "Aceito"},
+    EmRotaDeEntrega: {value: 3, descricao: "Em rota de entrega"},
+    ProntoParaRetirada: {value: 4, descricao: "Pronto para retirada"},
+    Finalizado: {value: 5, descricao: "Finalizado"},
+    Cancelado: {value: 6, descricao: "Cancelado"}
+});
