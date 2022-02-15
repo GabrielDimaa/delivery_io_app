9d6f96a80277c4febc2b24ce96077a51096d95e8
Gabriel de Matos Hainzenreder <44183128+GabrielDimaa@users.noreply.github.com>
2022-02-15 00:01:15 -0300

    Subcategoria Model

---
+++ src/models/subcategoriaModel.js
@@ -0,0 +1,40 @@
+export default class SubcategoriaModel {
+    constructor(
+        idSubcategoria,
+        descricao,
+        idCategoria,
+        deleted)
+    {
+        this.idSubcategoria = idSubcategoria;
+        this.descricao = descricao;
+        this.idCategoria = idCategoria;
+        this.deleted = deleted;
+    }
+
+    toJson() {
+        return {
+            "id_subcategoria": this.idSubcategoria,
+            "descricao": this.descricao,
+            "id_categoria": this.idCategoria,
+            "deleted": this.deleted
+        };
+    }
+
+    static fromJson(json) {
+        return new SubcategoriaModel(
+            json.id_subcategoria,
+            json.descricao,
+            json.id_categoria,
+            json.deleted
+        );
+    }
+
+    clone() {
+        return new SubcategoriaModel(
+            this.idSubcategoria,
+            this.descricao,
+            this.idCategoria,
+            this.deleted
+        );
+    }
+}
