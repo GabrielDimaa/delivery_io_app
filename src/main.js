65b72e45bc054780584d01555381c4f40a5f6231
Gabriel de Matos Hainzenreder <44183128+GabrielDimaa@users.noreply.github.com>
2022-02-24 20:48:23 -0300

    Web Sockets

--- src/main.js
+++ src/main.js
@@ -7,8 +7,19 @@ import './plugins/toasted.js';
 import './plugins/vuemask.js';
 import './plugins/vuetify-money.js';
 import 'es6-promise/auto.js';
+import Echo from 'laravel-echo';
 
 Vue.config.productionTip = false;
+
+window.Pusher = require('pusher-js');
+
+window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: 'local',
+  wsHost: '127.0.0.1',
+  wsPort: 6001,
+  forceTLS: false
+});
 
 new Vue({
   vuetify,
--- src/ui/App.vue
+++ src/ui/App.vue
@@ -5,6 +5,7 @@
 
         <v-main id="main">
             <Content/>
+            <Notification/>
         </v-main>
     </v-app>
 </template>
@@ -13,10 +14,37 @@
 import AppBar from "./templates/AppBar";
 import Drawer from "./templates/Drawer";
 import Content from "./templates/Content";
+import Notification from "./components/shared/Notification";
+import PedidoModel from "../models/pedidoModel";
+import {mapActions, mapState} from "vuex";
 
 export default {
     name: "App",
-    components: {Content, Drawer, AppBar}
+    components: {Content, Drawer, AppBar, Notification},
+    computed: {
+        ...mapState('pedidos', ['pedidos'])
+    },
+    methods: {
+        ...mapActions(["setShowNotification"]),
+        handleListen(event) {
+            const pedidoModel = PedidoModel.fromJson(event.pedido);
+            this.setShowNotification(true);
+
+            if (pedidoModel && pedidoModel.idPedido !== null) {
+                const existePedidoNaLista = this.pedidos.some(e => e.idPedido === pedidoModel.idPedido);
+
+                if (!existePedidoNaLista) {
+                    this.pedidos.unshift(pedidoModel);
+                }
+            }
+        }
+    },
+    mounted() {
+        window.Echo.channel('pedido')
+            .listen('.Pedido', (event) => {
+                this.handleListen(event);
+            });
+    }
 }
 </script>
 
